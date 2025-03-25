7eac892a0e0896af259d95a834c91a76ffa47d16
getinthere <getinthere@naver.com>
2025-03-25 12:36:01 +0900

    인증 로직 완료

--- src/main/java/com/metacoding/bankv1/user/UserController.java
+++ src/main/java/com/metacoding/bankv1/user/UserController.java
@@ -1,6 +1,6 @@
 package com.metacoding.bankv1.user;
 
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +10,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
     private final UserService userService;
+    private final HttpSession session;
+
+    @GetMapping("/logout")
+    public String logout() {
+        session.invalidate();
+        return "redirect:/";
+    }
+
+    // 로그인만 예외로 Post (조회시에도)
+    @PostMapping("/login")
+    public String login(UserRequest.LoginDTO loginDTO) {
+        User sessionUser = userService.로그인(loginDTO);
+        session.setAttribute("sessionUser", sessionUser); // stateful
+        return "redirect:/";
+    }
 
     @GetMapping("/login-form")
     public String loginForm() {
@@ -22,7 +37,7 @@ public class UserController {
     }
 
     @PostMapping("/join")
-    public String join(UserRequest.JoinDTO joinDTO, HttpServletRequest request) {
+    public String join(UserRequest.JoinDTO joinDTO) {
         userService.회원가입(joinDTO);
         return "redirect:/login-form";
     }
